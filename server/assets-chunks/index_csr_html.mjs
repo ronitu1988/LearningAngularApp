3bfe9b8ac042f7389fb727ee27642a9ef90e8e88
Roni Tu <mrt.csdeverloper@gmail.com>
2025-04-16 20:38:25 -0700

    Auto-generated commit

--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -1,9 +1,9 @@
-<!doctype html>
+export default `<!doctype html>
 <html lang="en" data-beasties-container="">
 <head>
   <meta charset="utf-8">
   <title>LearningAngularApp</title>
-  <base href="LearningAngularApp">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
@@ -11,3 +11,4 @@
   <app-root></app-root>
 <script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-FZLXJS3Y.js" type="module"></script></body>
 </html>
+`;
